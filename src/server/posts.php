a4e18a6faf1f82b97154ba9853d1acec28dcf9ad
Trystan <trystansiew@gmail.com>
2024-07-30 13:55:55 +0800

    changing all php allow origin to *

--- src/server/posts.php
+++ src/server/posts.php
@@ -1,5 +1,5 @@
 <?php
-    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     include("database.php");
     
