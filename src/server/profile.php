edafbe8971a7177580cc0fe02d280f1a4bc65649
Trystan <trystansiew@gmail.com>
2024-07-30 15:01:20 +0800

    commit

--- src/server/profile.php
+++ src/server/profile.php
@@ -1,6 +1,21 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
+    $allowed_origins = [
+        'http://localhost:3000',
+        'https://clonebook-iota.vercel.app'
+    ];
+
+    $origin = $_SERVER['HTTP_ORIGIN'];
+
+    if (in_array($origin, $allowed_origins)) {
+        header('Access-Control-Allow-Origin: ' . $origin);
+    } else {
+        header('HTTP/1.1 403 Forbidden');
+        exit('Forbidden');
+    }
+
     header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Headers: Content-Type');
+    
     session_start();
     include("database.php");
 
